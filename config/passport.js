1d554d52e4ace3ae1b94e901aefe0ba16cf715a2
NickCampbell91 <nickcampbell1991@gmail.com>
2022-07-02 21:11:06 -0600

    Add files via upload

---
+++ config/passport.js
@@ -0,0 +1,12 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
+
+passport.use(
+    new GoogleStrategy({
+        callbackURL: 'auth/google/redirect',
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.SECRET
+    }, (accessToken, refreshToken, profile, done) => {
+    })
+);
